b0b00b1144a9c963589d2ce855dbe73e4bcb845a
Vansh Khanna <74200673+VK7160@users.noreply.github.com>
2022-04-24 22:11:07 +0530

    Update device.js

--- Topic 6/api/models/device.js
+++ Topic 6/api/models/device.js
@@ -5,4 +5,4 @@ module.exports = mongoose.model('Device', new mongoose.Schema({
   name: String,
   user: String,
   sensorData: Array
-}, { collection : 'topic4' }));
+}, { collection : 'topic6' }));
