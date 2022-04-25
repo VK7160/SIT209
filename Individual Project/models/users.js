99b7332f0589f4735145c16f9067ae3c4570f3fd
Vansh Khanna <74200673+VK7160@users.noreply.github.com>
2022-04-26 02:20:45 +0530

    Add files via upload

--- Individual Project/models/users.js
+++ Individual Project/models/users.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+
+var mongoose = require("mongoose");
+module.exports = mongoose.model("users", userSchema({
+  email:
+  {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true
+  },
+  password:
+  {
+    type: String,
+    required: true
+  }
+}, {collection: 'individual-task'}));
+
 
